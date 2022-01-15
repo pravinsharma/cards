f2aed563fc99c7e8ddeebe45d13b7fd2d72ffeea
Praveen Kumar Sharma <pravinsharma@hotmail.com>
2022-01-15 21:22:20 +0530

    .

--- lib/cards.ex
+++ lib/cards.ex
@@ -9,6 +9,11 @@ defmodule Cards do
   ## Examples
 
       iex> Cards.create_deck
+      ["Ace of Spades", "Two of Spades", "Three of Spades", "Four of Spades",
+      "Five of Spades", "Ace of Clubs", "Two of Clubs", "Three of Clubs",
+      "Four of Clubs", "Five of Clubs", "Ace of Hearts", "Two of Hearts",
+      "Three of Hearts", "Four of Hearts", "Five of Hearts", "Ace of Diamonds",
+      "Two of Diamonds", "Three of Diamonds", "Four of Diamonds", "Five of Diamonds"]
 
   """
   def create_deck do
--- test/cards_test.exs
+++ test/cards_test.exs
@@ -2,7 +2,23 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-  test "create_deck" do
-    assert Cards.create_deck() != nil
+  test "creates a deck of 20 cards" do
+    assert length(Cards.create_deck()) == 20
+  end
+
+  test "shuffles a deck of cards" do
+    deck = Cards.create_deck
+    assert Cards.shuffle(deck) != deck
+  end
+
+  test "deal a deck of 5 cards" do
+    deck = Cards.create_deck
+    assert length(Cards.deal(deck, 5)) == 5
+  end
+
+  test "create a unique hand" do
+    hand1 = Cards.create_hand(4)
+    hand2 = Cards.create_hand(4)
+    assert hand1 != hand2
   end
 end
